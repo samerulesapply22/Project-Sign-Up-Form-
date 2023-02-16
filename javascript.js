c22b6f0ab338df05848cdef1e152acb6c21ca751
samerulesapply22 <curryballato@gmail.com>
2023-02-16 19:49:34 +0200

    Add confirm password function

--- javascript.js
+++ javascript.js
@@ -2,13 +2,15 @@ function match() {
     password = document.querySelector("#password").value;
     confirm_password = document.querySelector("#confirm-password").value;
     message = document.querySelector("#message");
+    button = document.querySelector("button")
     if (password == confirm_password) {
         message.textContent = "passwords match";
         message.style.color = "green";
+        button.disabled = false;
+        
     } else {
         message.textContent = "passwords do not mantch";
-        message.style.color = "red";
-        
+        message.style.color = "red";  
     }
 }
 
